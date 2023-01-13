0d6dfe6952f6ed9ff9a7937be502b7e14250a3f7
Damien <damien.deboudt@sfr.fr>
2023-01-13 13:53:33 +0100

    en cours

--- index.php
+++ index.php
@@ -6,51 +6,57 @@
 
 ## Exercice 1 Afficher la date courante en respectant la forme jj/mm/aaaa (ex : 16/05/2016)
 
+
+echo date("j/m/Y");
 echo "<br><br>";
 
 
 
 ## Exercice 2 Afficher la date courante en respectant la forme jj-mm-aa (ex : 16-05-16)
-
+echo date("j-m-y");
 echo "<br><br>";
 
 
 
 ## Exercice 3 Afficher la date courante avec le jour de la semaine et le mois en toutes lettres (ex : mardi 2 août 2016)
 ## Bonus : Le faire en français.
-
+setlocale(LC_ALL, ['fr', 'fra', 'fr_FR']);
+echo strftime("%A %d %B %Y");
 echo "<br><br>";
 
 
 
 ## Exercice 4 Afficher le timestamp du jour.
 ## Afficher le timestamp du mardi 2 août 2016 à 15h00.
-
+echo mktime(15, 0, 0, 8, 2, 2 );
 echo "<br><br>";
 
 
 
 ##Exercice 5 Afficher le nombre de jours qui séparent la date du jour avec le 16 mai 2016.
 
-echo "<br><br>";
-
-
-
 ##Exercice 6 Afficher le nombre de jours dans le mois de février de l'année 2016.
 
 echo "<br><br>";
 
-
+$number = cal_days_in_month(CAL_GREGORIAN, 2, 2023);
+echo $number;
 
 ##Exercice 7 Afficher la date du jour + 20 jours.
 
 echo "<br><br>";
 
+$dateTime = new DateTime("now");
+$dateTime->modify('+20 day');
+echo $dateTime->format('d m Y');
+
 ##Exercice 8 Afficher la date du jour - 22 jours
 
 echo "<br><br>";
 
-
+$dateTime = new DateTime("now");
+$dateTime->modify('-22 day');
+echo $dateTime->format('d m Y');
 ## TP Faire un formulaire avec deux listes déroulantes.
 ## La première sert à choisir le mois, et le deuxième permet d'avoir l'année.
 # MEGA BONUS ( surligné et caractère gras :-)  ) : En fonction des choix, afficher un calendrier ( voir libs JS ou autres )
